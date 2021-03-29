099567a3be8b66be35da12ba43fc7056d05c2ade
Justin Jaeger <jjustinjaeger@gmail.com>
2021-03-29 16:42:07 -0400

    preparing to launch

--- client/src/utils/sampleData.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  {date: "Sat Mar 27 2021", precipitation: 0, min: 48, max: 55},
-  {date: "Sun Mar 28 2021", precipitation: 0.53, min: 49, max: 61},
-  {date: "Mon Mar 29 2021", precipitation: 0.02, min: 41, max: 51},
-  {date: "Tue Mar 30 2021", precipitation: 0, min: 45, max: 51},
-  {date: "Wed Mar 31 2021", precipitation: 0.11, min: 47, max: 53},
-];
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
     plugins : [
         new HtmlWebpackPlugin ({
           template: './client/src/index.html',
+          filename: "./index.html"
         })
     ]
 
